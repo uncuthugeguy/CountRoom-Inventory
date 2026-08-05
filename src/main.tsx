e418bbc7a2cd09b3f486cc2342b8a6c5384284d2
uncuthugeguy <antonygowers90@gmail.com>
2026-08-05 20:42:24 +0100

    Replace App component with AuthGate

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
-import { App } from './App'
+import { AuthGate } from './AuthGate'
 import './styles.css'
 
 const container = document.getElementById('root')
@@ -9,7 +9,7 @@ if (!container) throw new Error('Missing #root element in index.html')
 
 createRoot(container).render(
   <StrictMode>
-    <App />
+    <AuthGate />
   </StrictMode>,
 )
 
